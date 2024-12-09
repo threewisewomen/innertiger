667d77e347e1b496409e27cf7d46cd197f74148b
John Mathew <jmathew@acilogistix.com>
2024-12-09 13:51:24 +0530

    Day 1 successful

---
+++ innertiger/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using innertiger.Challenges;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("To awken the inner tiger I will complete the Advent Code Challenges");
+        Console.WriteLine("Enter the day of the challenge you want to run");
+
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out int result))
+        {
+            switch (result)
+            {
+                case 1:
+                    new Day1().FindingDistance();
+                    break;
+                default:
+                    Console.WriteLine("Challenge for this day is not implemented yet.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid day number.");
+        }
+    }
+}
